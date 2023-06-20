7c1da54b0f30d662c21addbb188058d5fa27aadc
Thanathorn15 <thanathorn_ko@kkumail.com>
2023-06-20 20:25:38 +0700

    test

--- src/pages/GuestPage.jsx
+++ src/pages/GuestPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import LogoSideBar from "../Common/LogoSideBar";
+import Sidebar from "../components/Sidebar";
 
 function GuestPage() {
   return (
     <div className="grid grid-cols-4">
       <div>
-        <LogoSideBar />
+        {/* <LogoSideBar /> */}
+        <Sidebar/>
       </div>
       <div className=" col-span-2">
         <h1 className="text-3xl font-bold mt-4">Explore</h1>
