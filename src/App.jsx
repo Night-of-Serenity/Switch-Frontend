b1d9f4a0e7201b483cfc12504c47e23171f7f486
Thanathorn15 <thanathorn_ko@kkumail.com>
2023-07-02 20:19:43 +0700

    fIX:TEST delete

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import Router from "./routes/Router";
-import { useState } from "react";
 
 function App() {
 
