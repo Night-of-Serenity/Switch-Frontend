c5ea8889acc2d1e36f53f75de39ababf38fdcb16
Peerapat Udtha(Night) <peerapat.ud@gmail.com>
2023-07-03 12:01:07 +0700

    update chatMesasageBar

--- src/components/chat/ChatMessageBar.jsx
+++ src/components/chat/ChatMessageBar.jsx
@@ -51,9 +51,8 @@ export default function ChatMessageBar() {
                 />
             </div>
             <button
-                type="button"
+                type="submit"
                 className="relative -ml-px inline-flex items-center gap-x-1.5 rounded-r-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                // onClick={handleSendMessage}
             >
                 <BarsArrowUpIcon
                     className="-ml-0.5 h-5 w-5 text-gray-400"
